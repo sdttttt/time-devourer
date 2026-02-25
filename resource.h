31c7ae1dfa1aa0360909c53eb91feb22fe16bb29
z0h <42728902+sdttttt@users.noreply.github.com>
2026-02-25 17:38:29 +0800

    refactor: 精简资源内容

--- resource.h
+++ resource.h
@@ -1,17 +1,7 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ 生成的包含文件。
-// 供 timer.rc 使用
-//
+#ifndef RESOURCE_H
+#define RESOURCE_H
+
 #define IDI_ICON1                       105
 #define IDI_ICON                        105
 
-// Next default values for new objects
-// 
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        106
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
-#define _APS_NEXT_SYMED_VALUE           102
-#endif
-#endif
+#endif // RESOURCE_H
